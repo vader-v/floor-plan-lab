767e1059167f4cc6334fb20eb7f5135a07a6bc5d
amirb <amir.bullock9@gmail.com>
2023-05-01 22:55:47 -0400

    stub up bedroom component

---
+++ src/Bedroom.jsx
@@ -0,0 +1,7 @@
+const Bedroom = (props) => {
+  return ( 
+    <div>Bedroom</div>
+  )
+}
+
+export default Bedroom
--- src/FloorPlan.jsx
+++ src/FloorPlan.jsx
@@ -1,13 +1,17 @@
 import Kitchen from "./Kitchen"
 import LivingRoom from "./LivingRoom"
-
+import Bedroom from "./Bedroom"
 
 const FloorPlan = (props) => {
   return ( 
     <>
-    <div>Floor Plan</div>
+    <div>Floor Plan
     <Kitchen />
     <LivingRoom />
+    <Bedroom />
+    <Bedroom />
+    <Bedroom />
+    </div>
     </>
   )
 }
