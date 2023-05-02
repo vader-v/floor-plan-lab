4ba84e3d0b5db41d375d99018471e1f5796c7136
amirb <amir.bullock9@gmail.com>
2023-05-01 20:39:38 -0400

    stub up kitchen and living room components

---
+++ src/Kitchen.jsx
@@ -0,0 +1,7 @@
+const Kitchen = (props) => {
+  return ( 
+    <h2>This is kitchen</h2>
+  )
+}
+
+export default Kitchen
---
+++ src/LivingRoom.jsx
@@ -0,0 +1,7 @@
+const LivingRoom = (props) => {
+  return ( 
+    <h4>This is living room</h4>
+  )
+}
+
+export default LivingRoom
